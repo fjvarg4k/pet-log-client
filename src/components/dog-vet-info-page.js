235ae69cf92eb9a0e38c9527d9295fabf079800e
Francisco Vargas <fjvarg4934@gmail.com>
2018-11-17 10:26:33 -0600

    Added displayAddVetBtnOrVetInfo

--- src/components/dog-vet-info-page.js
+++ src/components/dog-vet-info-page.js
@@ -9,35 +9,42 @@ export class DogVetInfoPage extends React.Component {
     this.props.dispatch(fetchDogById(this.props.match.params.dogid));
   }
 
-  // displayAddBtnOrVetInfo() {
-  //   if (this.props.dogInfo.vetName === '') {
-  //     return (
-  //       <Link to="/add-new-vet">Add Vet Info</Link>
-  //     );
-  //   }
-  //   return this.checkVetInfo();
-  // }
-
-  checkVetInfo() {
-    if (this.props.dogInfo !== undefined) {
+  displayAddBtnOrVetInfo() {
+    if (this.props.dogInfo !== undefined && this.props.dogInfo.vetName === '') {
+      return (
+        <Link to="/add-new-vet">Add Vet Info</Link>
+      );
+    } else if (this.props.dogInfo !== undefined){
       return (
         <div>
           <p>Veterinarian Name: {this.props.dogInfo.vetName}</p>
           <p>Vet Location Name: {this.props.dogInfo.vetLocationName}</p>
           <p>Address: {this.props.dogInfo.address}</p>
+          <Link to="/edit-vet-info">Edit Vet Info</Link>
         </div>
       );
     }
   }
+
+  // checkVetInfo() {
+  //   if (this.props.dogInfo !== undefined) {
+  //     return (
+  //       <div className="vet-details">
+  //         <p>Veterinarian Name: {this.props.dogInfo.vetName}</p>
+  //         <p>Vet Location Name: {this.props.dogInfo.vetLocationName}</p>
+  //         <p>Address: {this.props.dogInfo.address}</p>
+  //         <Link to="/edit-vet-info">Edit Vet Info</Link>
+  //       </div>
+  //     );
+  //   }
+  // }
 
   render() {
     return (
       <div className="dog-vet-info">
         <Link to="/dashboard">Back</Link>
         <h6>Vet Info</h6>
-        <Link to="/add-new-vet">Add Vet Info</Link>
-        {this.checkVetInfo()}
-        <Link to="/edit-vet-info">Edit Vet Info</Link>
+        {this.displayAddBtnOrVetInfo()}
       </div>
     );
   }
--- src/components/dog.js
+++ src/components/dog.js
@@ -2,16 +2,23 @@ import React from 'react';
 // import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
-const Dog = (props) => {
-  return (
-    <div className="dog-overview">
-      <h3>{props.dog.name}</h3>
-      {/* <button>Delete Info</button> */}
-      <button onClick={() => props.history.push(`/dog-details/${props.dog.id}`)}>View Dog Info</button>
-      <button onClick={() => props.history.push(`/dog-medication/${props.dog.id}`)}>View Dog Medications </button>
-      <button onClick={() => props.history.push(`/vet-info/${props.dog.id}`)}>View Vet Info</button>
-    </div>
-  );
+export class Dog extends React.Component {
+  // handleVetBtn() {
+  //   if (condition) {
+  //
+  //   }
+  // }
+
+  render() {
+    return (
+      <div className="dog-overview">
+        <h3>{this.props.dog.name}</h3>
+        <button onClick={() => this.props.history.push(`/dog-details/${this.props.dog.id}`)}>View Dog Info</button>
+        <button onClick={() => this.props.history.push(`/dog-medication/${this.props.dog.id}`)}>View Dog Medications </button>
+        <button onClick={() => this.props.history.push(`/vet-info/${this.props.dog.id}`)}>View Vet Info</button>
+      </div>
+    );
+  }
 }
 
 export default (withRouter)(Dog);
