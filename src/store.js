4e5ab76af079435f903634da8509ff2bd0436866
Francisco Vargas <fjvarg4934@gmail.com>
2018-11-19 15:43:17 -0600

    Removed Redux devtool extension

--- src/store.js
+++ src/store.js
@@ -7,19 +7,6 @@ import dogReducer from './reducers/dog';
 import medicationReducer from './reducers/medication';
 import { setAuthToken, refreshAuthToken } from './actions/auth';
 
-const store = createStore(
-  combineReducers({
-    form: formReducer,
-    auth: authReducer,
-    dog: dogReducer,
-    medication: medicationReducer
-  }),
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
-
 // const store = createStore(
 //   combineReducers({
 //     form: formReducer,
@@ -27,8 +14,21 @@ const store = createStore(
 //     dog: dogReducer,
 //     medication: medicationReducer
 //   }),
-//   applyMiddleware(thunk)
+//   compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
 // );
+
+const store = createStore(
+  combineReducers({
+    form: formReducer,
+    auth: authReducer,
+    dog: dogReducer,
+    medication: medicationReducer
+  }),
+  applyMiddleware(thunk)
+);
 
 // Loads up auth token if it exists in localStorage
 const authToken = loadAuthToken();
