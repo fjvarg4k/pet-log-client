1e937c29e6ee9d2c1a7381b9337584e0e739573d
Francisco Vargas <fjvarg4934@gmail.com>
2019-01-14 17:52:16 -0600

    Added email to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,7 @@ export default class Footer extends React.Component {
       <p className="footer-title item-heading">Created By Francisco Vargas</p>
       <a href="https://www.linkedin.com/in/francisco-vargas-246414169/"><i className="fab fa-linkedin footer-icon"></i></a>
       <a href="https://github.com/fjvarg4k"><i className="fab fa-github-square footer-icon"></i></a>
-      <a href="https://twitter.com/franpanchomon"><i className="fab fa-twitter-square footer-icon"></i></a>
+      <a href="mailto:fjvarg4934@gmail.com"><i className="fas fa-envelope-square footer-icon"></i></a>
       </footer>
     );
   }
